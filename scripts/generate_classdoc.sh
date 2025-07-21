49b91dc831d34e2a755125b877e34e973d9063f8
Hahn Axel (hahn) <axel.hahn@unibe.ch>
2025-07-21 10:56:21 +0200

    generator script: add source param

--- scripts/generate_classdoc.sh
+++ scripts/generate_classdoc.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
+# ====================================================================== 
+#
+# Generator for markdown files from php class files
+#
+# ----------------------------------------------------------------------
+# 2025-07-21  v1.0  axelhahn  initial version
+# ====================================================================== 
 
 cd "$( dirname $0)/.."
 
-./parse-class.php --debug --out md src/phpclass-parser.class.php > docs/70_Class.md
+files="
+    src/phpclass-parser.class.php
+"
+outdir="docs/70_Classes"
+sourceurl="https://github.com/axelhahn/php-classdoc//blob/main"
+
+# ----------------------------------------------------------------------
+# MAIN
+# ----------------------------------------------------------------------
+
+for myfile in $files
+do
+    outfile="$outdir/$(basename "$myfile").md"
+
+    ./parse-class.php \
+        --debug \
+        --source "$sourceurl/$myfile" \
+        --out md \
+        "$myfile" > "$outfile"
+done
+
+# ----------------------------------------------------------------------

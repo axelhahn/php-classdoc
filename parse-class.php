440b14877630315ccaf59042a340b0bc487d9c78
Hahn Axel (hahn) <axel.hahn@unibe.ch>
2026-01-27 10:29:01 +0100

    fix missing sourceurl

--- parse-class.php
+++ parse-class.php
@@ -145,7 +145,7 @@ if(!$sClass){
     _wd("Init parser with param 2: $sClass");
     $oParser->setClassname($sClass);
 }
-if($sourceurl){
+if($sourceurl??false){
     $oParser->setSourceUrl($sourceurl);
 }
 // ---------- generate output
